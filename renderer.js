783a939a67b151a37636380ebb5c9af4fb6ddc10
Joshua Cadavez <jcadavez@insider.com>
2022-07-09 23:55:27 -0400

    Drag and drop functionality

--- renderer.js
+++ renderer.js
@@ -7,3 +7,13 @@ const func = async () => {
 }
 
 func()
+
+document.getElementById('drag1').ondragstart = (event) => {
+    event.preventDefault()
+    window.electron.startDrag('drag-and-drop-1.md')
+}
+  
+document.getElementById('drag2').ondragstart = (event) => {
+    event.preventDefault()
+    window.electron.startDrag('drag-and-drop-2.md')
+}
